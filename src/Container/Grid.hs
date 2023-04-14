a181648e19e225d9aff58b72287904a1980e4d5b
Paul Schnapp <paul.schnapp@gmail.com>
2023-04-14 16:02:29 -0400

    Finished Grid implementation

--- src/Container/Grid.hs
+++ src/Container/Grid.hs
@@ -6,6 +6,7 @@ module Container.Grid
 import Container
 import Container.Item
 import Internal.Util
+import Layout
 
 
 data Grid k
@@ -21,11 +22,27 @@ instance Container (Grid k) k where
   requiredHeight (Grid r _ is) p =
     fmap (* r) . getMaxSize $ map (`requiredHeight` p) is
   addToLayout (Grid r c is) bounds renderGroup = do
-    -- TODO:
-    -- make guides for each grid item and combine them into bounds
-    -- (make elastics and symmetric one over/down for neighboring positions)
-    -- zip bounds with `is` and run through mapM_ below
-    mapM_ (\i -> addToLayout i bounds renderGroup) is
+    let addBetween f1 f2 p =
+          addGuideToLayout $ Between (f1 bounds, p) (f2 bounds, 1-p)
+    elasHorizs <- mapM (addBetween topOf bottomOf) (percents r)
+    elasVerts <- mapM (addBetween leftOf rightOf) (percents c)
+    let addSymNeighbor g =
+          addGuideToLayout $ Relative 1 g Symmetric
+    relHorizs <- mapM addSymNeighbor elasHorizs
+    relVerts <- mapM addSymNeighbor elasVerts
+    let tops = topOf bounds : relHorizs
+    let lefts = leftOf bounds : relVerts
+    let rights = elasVerts ++ [rightOf bounds]
+    let bottoms = elasHorizs ++ [bottomOf bounds]
+    let boundsForItems =
+          [Bounds t l r b
+          | (t, b) <- zip tops bottoms
+          , (l, r) <- zip lefts rights
+          ]
+    mapM_ (\(i, b) -> addToLayout i b renderGroup) (zip is boundsForItems)
+
+percents :: Int -> [Float]
+percents n = fmap (\i -> fromIntegral i / fromIntegral n) [1..n-1]
 
 grid :: Int -> Int -> [Item k] -> Grid k
 grid = Grid

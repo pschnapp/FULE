c9ea32598979e5427c22084e1d86c3a174e9d421
Paul Schnapp <paul.schnapp@gmail.com>
2023-06-01 11:16:55 -0400

    Renamed Overflowed -> Overflow

--- src/FULE/Container/Overflow.hs
+++ src/FULE/Container/Overflow.hs
@@ -1,11 +1,11 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 
-module FULE.Container.Overflowed
- ( Overflowed
- , overflowedHoriz
- , overflowedVert
- , overflowed
+module FULE.Container.Overflow
+ ( Overflow
+ , overflowHoriz
+ , overflowVert
+ , overflow
  ) where
 
 import Control.Monad.Trans.Class
@@ -17,16 +17,16 @@ import FULE.Internal.Direction
 import FULE.Layout
 
 
-data Overflowed c = Overflowed (Maybe Direction) c
+data Overflow c = Overflow (Maybe Direction) c
 
-instance (Container c k m) => Container (Overflowed c) k m where
-  minWidth (Overflowed dir c) proxy = case dir of
+instance (Container c k m) => Container (Overflow c) k m where
+  minWidth (Overflow dir c) proxy = case dir of
     Just Vertical -> minWidth c proxy
     _ -> return Nothing
-  minHeight (Overflowed dir c) proxy = case dir of
+  minHeight (Overflow dir c) proxy = case dir of
     Just Horizontal -> minHeight c proxy
     _ -> return Nothing
-  addToLayout (Overflowed dir c) proxy bounds renderGroup = do
+  addToLayout (Overflow dir c) proxy bounds renderGroup = do
     reqWidth <- lift . lift $ minWidth c proxy
     reqHeight <- lift . lift $ minHeight c proxy
     let Bounds t l r b = bounds
@@ -35,12 +35,12 @@ instance (Container c k m) => Container (Overflowed c) k m where
     let bounds = Bounds t l right bottom
     addToLayout c proxy bounds renderGroup
 
-overflowedHoriz :: c -> Overflowed c
-overflowedHoriz = Overflowed (Just Horizontal)
+overflowHoriz :: c -> Overflow c
+overflowHoriz = Overflow (Just Horizontal)
 
-overflowedVert :: c -> Overflowed c
-overflowedVert = Overflowed (Just Vertical)
+overflowVert :: c -> Overflow c
+overflowVert = Overflow (Just Vertical)
 
-overflowed :: c -> Overflowed c
-overflowed = Overflowed Nothing
+overflow :: c -> Overflow c
+overflow = Overflow Nothing
 

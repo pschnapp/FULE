40f0844f2d77e08545281bca9514d31755814701
Paul Schnapp <paul.schnapp@gmail.com>
2023-08-18 11:57:45 -0400

    Documented Layered module

--- src/FULE/Container/Layered.hs
+++ src/FULE/Container/Layered.hs
@@ -15,13 +15,16 @@ import FULE.Container.Item
 import FULE.Internal.Util
 
 
+-- | This container layers multiple visual 'FULE.Container.Item.ItemM's within
+--   the same bounding rectangle, one on top of the other. Z-ordering is not
+--   really taken into account since that depends on how you are using the
+--   layout output, but the @ItemM@s will appear in the output in the same order
+--   they're in when passed to the 'layered' function.
 newtype LayeredM m k = Layered [ItemM m k]
 
+-- | Like 'LayeredM' but run in the 'Data.Functor.Identity.Identity' monad.
 type Layered = LayeredM Identity
 
--- TODO
---  - render order
---  - input censoring
 instance (Monad m) => Container (LayeredM m k) k m where
   minWidth (Layered is) p = getMaxSize <$> mapM (`minWidth` p) is
   minHeight (Layered is) p = getMaxSize <$> mapM (`minHeight` p) is
@@ -29,6 +32,7 @@ instance (Monad m) => Container (LayeredM m k) k m where
     renderGroup' <- Just <$> maybe nextRenderGroup pure renderGroup
     mapM_ (\i -> addToLayout i proxy bounds renderGroup') is
 
+-- | Layer 'FULE.Container.Item.ItemM's within the same bounding rectangle.
 layered :: [ItemM m k] -> LayeredM m k
 layered = Layered
 

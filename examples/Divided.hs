b0ea1805a1e00da29cb749fc7a1bf4f87214d7ec
Paul Schnapp <paul.schnapp@gmail.com>
2023-09-18 13:43:32 -0400

    Added some example code

---
+++ examples/Divided.hs
@@ -0,0 +1,61 @@
+-- This module contains two examples of Divided containers (using `sizedTop`):
+--  - one statically divided
+--  - one dynamically divided
+--
+-- Note the type-annotation on the `Dynamics` for each divided container:
+-- this is required since the type-checker cannot disambiguate `Widget`
+-- as the type-parameter to `Dynamics` by itself.
+-- Using the qualifiers here requires the ScopedTypeVariables language-extension.
+-- You could also declare these values outside of the function bodies and
+-- not need to use the language-extension.
+--
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Divided where
+
+import FULE
+
+
+data Widget
+  = ResizeControl
+    { widthOf :: Int
+    , heightOf :: Int
+    -- These guides should be used to resize the `Layout` in response to
+    -- the window being resized, using a delta.
+    -- Note that you will have to wire this to the GUI framework yourself.
+    , widthGuideOf :: GuideID
+    , heightGuideOf :: GuideID
+    }
+  | UnsizedContent
+  | SizedContent
+  | DividerBar
+    { orientationOf :: Orientation
+    , movementGuideOf :: GuideID
+    }
+
+getWindowSize :: m (Int, Int)
+getWindowSize = undefined -- for you to implement
+
+staticDivided :: (Monad m) => m (Layout, [ComponentInfo Widget])
+staticDivided = do
+  (width, height) <- getWindowSize
+  let staticSize :: Dynamics Widget = static
+  return $ layout
+    (window width height (ResizeControl width height)
+      (sizedTop sizedToContents staticSize
+        SizedContent
+        UnsizedContent
+        ))
+
+dynamicDivided :: (Monad m) => m (Layout, [ComponentInfo Widget])
+dynamicDivided = do
+  (width, height) <- getWindowSize
+  let dynamicSize :: Dynamics Widget = dynamic DividerBar 10
+  return $ layout
+    (window width height (ResizeControl width height)
+      (sizedTop (sizedTo 100) dynamicSize
+        -- Depending on the content, you may wish to use `clipped` to prevent
+        -- it from displaying out of bounds when the container is resized.
+        (clipped SizedContent)
+        (clipped UnsizedContent)
+        ))
---
+++ examples/Grid.hs
@@ -0,0 +1,59 @@
+-- This module contains example usages of `Grid`, `GridM`, `Item`, and `ItemM`.
+--
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Grid where
+
+import FULE
+
+
+data Widget
+  = ResizeControl
+    { widthOf :: Int
+    , heightOf :: Int
+    -- These guides should be used to resize the `Layout` in response to
+    -- the window being resized, using a delta.
+    -- Note that you will have to wire this to the GUI framework yourself.
+    , widthGuideOf :: GuideID
+    , heightGuideOf :: GuideID
+    }
+  | Content String
+
+
+exampleGrid :: Int -> Int -> (Layout, [ComponentInfo Widget])
+exampleGrid width height = layout
+  (window width height (ResizeControl width height)
+    (grid 2 3
+      -- Annoyingly a type-annotation is required for the list of items which
+      -- must be contained within an extra set of parentheses.
+      ([item (Content "A")
+      , item (Content "B")
+      , item (Content "C")
+      , item (Content "D")
+      , item (Content "E")
+      , item (Content "F")
+      ]::[Item Widget])))
+
+
+getWindowSize :: m (Int, Int)
+getWindowSize = undefined -- for you to implement
+
+-- When using `ItemM` you'll need to specify a `forall` for your monadic
+-- type-variable in the type-signature and use the `ScopedTypeVariables`
+-- language-extension to get the type-checker to use the same variable for the
+-- type-signature and the type-annotation of your `ItemM` list in the body.
+exampleGridM :: forall m . (Monad m) => m (Layout, [ComponentInfo Widget])
+exampleGridM = do
+  (width, height) <- getWindowSize
+  layoutM
+    (window width height (ResizeControl width height)
+      (grid 2 3
+        -- Annoyingly a type-annotation is required for the list of items which
+        -- must be contained within an extra set of parentheses.
+        ([item (Content "A")
+        , item (Content "B")
+        , item (Content "C")
+        , item (Content "D")
+        , item (Content "E")
+        , item (Content "F")
+        ]::[ItemM m Widget])))

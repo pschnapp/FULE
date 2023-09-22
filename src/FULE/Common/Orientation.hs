2ba356a09a7c1bac2b3c99641c86ad5a3b752988
Paul Schnapp <paul.schnapp@gmail.com>
2023-09-22 10:43:05 -0400

    Decided to keep NFData instance

--- src/FULE/Common/Orientation.hs
+++ src/FULE/Common/Orientation.hs
@@ -1,6 +1,11 @@
 module FULE.Common.Orientation where
 
+import Control.DeepSeq
+
 
 -- | Layout orientation, used in multiple 'FULE.Container.Container's.
 data Orientation = Horizontal | Vertical
+
+instance NFData Orientation where
+  rnf o = seq o ()
 
